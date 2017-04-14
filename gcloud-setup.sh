816e486bd1604fc54e1c110f9affb5af94f45b26
Aaron Culich <aculich@gmail.com>
2017-04-13 19:27:31 -0700

    don't need chown ~/.config with gcloud SDK 149.0.0

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -57,7 +57,7 @@ gcloud config get-value project
 gcloud config set compute/zone ${ZONE}
 gcloud config get-value compute/zone
 time gcloud container clusters create ${CLUSTER_NAME} --num-nodes=${NUM_NODES}
-sudo chown -R $USER $HOME/.config
+#sudo chown -R $USER $HOME/.config
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sudo bash
 echo "Run this in another tab while helm install is --wait'ing"
 echo "kubectl --namespace=${NAMESPACE} get pod; kubectl --namespace=${NAMESPACE} get svc" 
