b1e722c2680a32ded1c7c85ed5e5fad45d7e4f12
Aaron Culich <aculich@gmail.com>
2017-04-18 22:17:44 -0700

    set -x

--- gcloud-bootstrap.sh
+++ gcloud-bootstrap.sh
@@ -55,9 +55,10 @@ fi
 
 cat <<EOF
 project=${project}
+EOF
 
+set -x
 
 gcloud compute --project "${project}" instances create "jhub-controller" --zone "us-central1-f" --machine-type "g1-small" --subnet "default" --maintenance-policy "MIGRATE" --service-account "1048240305778-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/cloud-platform" --tags "http-server","https-server" --image "ubuntu-1610-yakkety-v20170330" --image-project "ubuntu-os-cloud" --boot-disk-size "20" --boot-disk-type "pd-ssd" --boot-disk-device-name "jhub-controller"
-EOF
 
 # gcloud compute --project "${project}" instances create "jhub-controller" --zone "us-central1-f" --machine-type "g1-small" --subnet "default" --metadata "startup-script=https://raw.githubusercontent.com/yuvipanda/binder-deployment/master/setup.bash" --maintenance-policy "MIGRATE" --service-account "1048240305778-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/cloud-platform" --tags "http-server","https-server" --image "ubuntu-1610-yakkety-v20170330" --image-project "ubuntu-os-cloud" --boot-disk-size "20" --boot-disk-type "pd-ssd" --boot-disk-device-name "jhub-controller"
