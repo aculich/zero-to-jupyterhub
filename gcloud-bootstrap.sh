3d8dbf4e09d0f32cdef2fc58feb315f953515f93
Aaron Culich <aculich@gmail.com>
2017-04-18 22:58:31 -0700

    update bootstrap and controller-setup

--- gcloud-bootstrap.sh
+++ gcloud-bootstrap.sh
@@ -58,7 +58,10 @@ project=${project}
 EOF
 
 set -x
+project_number=$(curl "http://metadata.google.internal/computeMetadata/v1/project/numeric-project-id" -H "Metadata-Flavor: Google")
+service_account="${project_number}-compute@developer.gserviceaccount.com"
+startup_script="https://raw.githubusercontent.com/aculich/zero-to-jupyterhub/master/gcloud-controller-setup.sh"
 
-gcloud compute --project "${project}" instances create "jhub-controller" --zone "us-central1-f" --machine-type "g1-small" --subnet "default" --maintenance-policy "MIGRATE" --service-account "1048240305778-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/cloud-platform" --tags "http-server","https-server" --image "ubuntu-1610-yakkety-v20170330" --image-project "ubuntu-os-cloud" --boot-disk-size "20" --boot-disk-type "pd-ssd" --boot-disk-device-name "jhub-controller"
+gcloud compute --project "${project}" instances create "jhub-controller" --zone "us-central1-f" --machine-type "g1-small" --subnet "default" --metadata "startup-script=${startup_script}" --maintenance-policy "MIGRATE" --service-account "${service_account}" --scopes "https://www.googleapis.com/auth/cloud-platform" --tags "bootstrapped" --image "ubuntu-1610-yakkety-v20170330" --image-project "ubuntu-os-cloud" --boot-disk-size "20" --boot-disk-type "pd-ssd" --boot-disk-device-name "jhub-controller"
 
 # gcloud compute --project "${project}" instances create "jhub-controller" --zone "us-central1-f" --machine-type "g1-small" --subnet "default" --metadata "startup-script=https://raw.githubusercontent.com/yuvipanda/binder-deployment/master/setup.bash" --maintenance-policy "MIGRATE" --service-account "1048240305778-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/cloud-platform" --tags "http-server","https-server" --image "ubuntu-1610-yakkety-v20170330" --image-project "ubuntu-os-cloud" --boot-disk-size "20" --boot-disk-type "pd-ssd" --boot-disk-device-name "jhub-controller"
--- gcloud-controller-setup.sh
+++ gcloud-controller-setup.sh
@@ -5,4 +5,18 @@ set -e
 PROGNAME=$(basename $0)
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-apt-get purge --yes google-cloud-sdk kubectl
+
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk=140.0.0-0ubuntu1~16.10 kubectl
+
+project=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+zone=$(curl --silent "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google" | cut -d/ -f4)
