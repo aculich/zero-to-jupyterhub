d9815d5c35bb201be24e6b459366f652909242c4
Aaron Culich <aculich@gmail.com>
2017-04-23 19:15:07 -0700

    add s2i into process

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -10,6 +10,10 @@ CLUSTER_NAME=test-cluster-a
 CHARTNAME=jhub-1
 ZONE=us-central1-f
 
+project=$(curl --silent "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+zone=$(curl --silent "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google" | cut -d/ -f4)
+repo=$( curl --silent "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true" -H "Metadata-Flavor: Google" | jq -r '.repo')
+
 
 ## uncomment below to override above settings if you want a larger cluster
 
@@ -26,6 +30,7 @@ ZONE=us-central1-f
 rm -rf ~/zero-to-jupyterhub
 cd $HOME
 git clone https://github.com/aculich/zero-to-jupyterhub/
+
 cd $HOME/zero-to-jupyterhub
 cat > gcloud-config.sh <<EOF
 NUM_NODES=${NUM_NODES}
@@ -35,6 +40,8 @@ CLUSTER_NAME=${CLUSTER_NAME}
 CHARTNAME=${CHARTNAME}
 ZONE=${ZONE}
 EOF
+cat gcloud-config.sh
+. gcloud-config.sh
 
 hubCookieSecret=$(openssl rand -hex 32)
 tokenProxy=$(openssl rand -hex 32)
@@ -46,25 +53,43 @@ token:
     # output of second execution of 'openssl rand -hex 32'
     proxy: "${tokenProxy}"
 singleuser:
-    image:
-       name: jupyter/datascience-notebook
-       tag: latest
+  image:
+    name: gcr.io/${project}/default-image
+    tag: latest
+storage:
+  type: none
 EOF
 cat config.yaml
+
+git clone https://github.com/aculich/s2i-builders
+cd $HOME/s2i-builders/singleuser-builder
+make
+s2i build ${repo} aculich/singleuser-builder:$(cat version) gcr.io/${project}/default-image:latest
+gcloud docker -- push gcr.io/${project}/default-image:latest
+
+
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 sudo gcloud components update --quiet --version=149.0.0
 sudo gcloud components install kubectl
-gcloud auth login
-gcloud auth application-default login
+## logins are not required when running on gcp instances
+#gcloud auth login
+#gcloud auth application-default login
 gcloud config set project ${DEVSHELL_PROJECT_ID}
 gcloud config get-value project
-time gcloud container clusters create ${CLUSTER_NAME} --num-nodes=${NUM_NODES} --zone=${ZONE}
+
+time gcloud container clusters create ${CLUSTER_NAME} --project ${project} --num-nodes=${NUM_NODES} --zone=${ZONE}
+
+
 sudo chown -R $USER $HOME/.config || echo "ignoring chown error"
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sudo bash
 echo "Run this in another tab while helm install is --wait'ing"
-echo "kubectl --namespace=${NAMESPACE} get pod; kubectl --namespace=${NAMESPACE} get svc" 
+echo "kubectl --namespace=${NAMESPACE} get pod; kubectl --namespace=${NAMESPACE} get svc"
 helm init
+
 JUPYTER_CHART=https://github.com/jupyterhub/helm-chart/releases/download/v0.1/jupyterhub-0.1.tgz
 helm install --wait ${JUPYTER_CHART} --name=${CHARTNAME} --namespace=${NAMESPACE} -f config.yaml
+
+# helm upgrade ${CHARTNAME} ${JUPYTER_CHART} -f config.yaml
+
 # gcloud container clusters get-credentials binder-cluster-dev --zone=us-central1-a
 
